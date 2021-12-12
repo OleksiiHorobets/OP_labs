5f6e128947e10b057fc72133c4263889a533c664
cmdax <90183750+cmdax@users.noreply.github.com>
2021-12-12 20:11:49 +0200

    Add files via upload

--- Lab_9/Op_lab_9.py
+++ Lab_9/Op_lab_9.py
@@ -1,4 +1,14 @@
+def isUniqueCheck(str, index):
+    isUnique = True
+    i = 0
+    while i<len(str) and isUnique:
+        if str[i] == str[index] and i != index:
+            isUnique = False
+        i+=1
+    return isUnique
+            
+
 str = input("Enter string: ")
 for i in range(0, len(str)):
-    if str.count(str[i]) == 1:
+    if isUniqueCheck(str, i):
         print("Unique char index: ", i)
